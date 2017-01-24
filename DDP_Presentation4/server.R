9984b023949700bd385f54c02d294ca184274a79
robertsg62 <robertsg62@users.noreply.github.com>
2017-01-24 05:59:36 -0600

    Add files via upload

--- DDP_Presentation4/server.R
+++ DDP_Presentation4/server.R
@@ -1,21 +1,25 @@
 server <- function(input, output) {
     df <- read.csv("ZonAnn.Ts.csv")
 
+    df1 <- reactive({df %>% filter(Year >= input$range1[1] & Year <= input$range1[2])})
+    df2 <- reactive({df %>% filter(Year >= input$range2[1] & Year <= input$range2[2])})
+    df3 <- reactive({df %>% filter(Year >= input$range3[1] & Year <= input$range3[2])})
+    
     output$plot1 <- renderPlotly({
-        p1 <- plot_ly(df, x = ~Year, y = ~Glob, color=~Year, type="bar") 
-        p1 %>% layout(title="Global", xaxis=list(title="Year")
-                      , yaxis=list(title="Annual Means"))
+        plot_ly(df1(), x = ~Year, y = ~Glob, color=~Year, type="bar") %>% 
+        layout(title="Global", xaxis=list(title="Year"), 
+               yaxis=list(title="Annual Means"))
     })
 
     output$plot2 <- renderPlotly({
-        p2 <- plot_ly(df, x = ~Year, y = ~NHem, color=~Year, type="bar") 
-        p2 %>% layout(title="Northern Hemisphere", xaxis=list(title="Year")
-                      , yaxis=list(title="Annual Means"))
+        plot_ly(df2(), x = ~Year, y = ~NHem, color=~Year, type="bar") %>% 
+        layout(title="Northern Hemisphere", xaxis=list(title="Year"), 
+               yaxis=list(title="Annual Means"))
     })
 
     output$plot3 <- renderPlotly({
-        p3 <- plot_ly(df, x = ~Year, y = ~SHem, color=~Year, type="bar") 
-        p3 %>% layout(title="Southern Hemisphere", xaxis=list(title="Year")
-                      , yaxis=list(title="Annual Means"))
+        plot_ly(df3(), x = ~Year, y = ~SHem, color=~Year, type="bar") %>% 
+        layout(title="Southern Hemisphere", xaxis=list(title="Year"), 
+               yaxis=list(title="Annual Means"))
     })
 }
--- DDP_Presentation4/ui.R
+++ DDP_Presentation4/ui.R
@@ -5,16 +5,23 @@ ui <- fluidPage(
     titlePanel("Global Mean Estimates Based on Land-Surface Air Temperature Anomalies Only"),
     sidebarLayout(
         sidebarPanel(
-            em("Moving the cursor over the bar charts will display the year and median differences for the year selected.")
+             em("Move cursor over the bar charts to median differences per year.")
+            ,br(),br(),br(),br()
+            ,sliderInput("range1", "Global Range:", min = 1880, max = 2015, value = c(1880, 2015), step = 1)
+            ,br(),br()
+            ,sliderInput("range2", "Northern Hemisphere Range:", min = 1880, max = 2015, value = c(1880, 2015), step = 1)
+            ,br(),br()
+            ,sliderInput("range3", "Southern Hemisphere Range:", min = 1880, max = 2015, value = c(1880, 2015), step = 1)
+            ,width=3
         ),
         mainPanel(
             br(),br(),
-            plotlyOutput("plot1", width="900px", height="600px"),
+            plotlyOutput("plot1", width="800px", height="200px"),
             br(),br(),
-            plotlyOutput("plot2", width="900px", height="600px"),
+            plotlyOutput("plot2", width="800px", height="200px"),
             br(),br(),
-            plotlyOutput("plot3", width="900px", height="600px"),
-            br(),br()
+            plotlyOutput("plot3", width="800px", height="200px"),
+            br(),br(),width=6
         )
     )
 )
